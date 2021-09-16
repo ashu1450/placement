ce4ef7b0f730ccf1e8995c510973580141911bca
Kavya Yadav <88396533+Kavya933@users.noreply.github.com>
2021-09-16 11:23:33 +0545

    server started and connected to mongo

--- server/server.js
+++ server/server.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const connectToMongo = require('./mongodb');
-
 connectToMongo();
+
+const app = express();
+const PORT = 6000;
+app.get('/',(req,res) =>{
+    res.send('hello kavya')
+})
+app.listen(PORT, ()=> console.log (`server is listining at http://localhost:${PORT}`));
